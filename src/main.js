1b5ca4cefc8f3e0cf4acbbba0893e86e534968d1
Farhad Azarbarzinniaz <farhad00niaz@gmail.com>
2021-02-26 13:57:39 +0330

    add highchart

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import Pages from "./pages/index";
 
+import HighchartsVue from "highcharts-vue";
+
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import "bootstrap/dist/css/bootstrap.css";
@@ -10,6 +12,8 @@ import "bootstrap-vue/dist/bootstrap-vue.css";
 Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin);
+
+Vue.use(HighchartsVue);
 
 Vue.config.productionTip = false;
 
