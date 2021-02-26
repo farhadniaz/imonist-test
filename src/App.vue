8d524d878491356a72d35262f104571e8841223a
Farhad Azarbarzinniaz <farhad00niaz@gmail.com>
2021-02-26 11:44:45 +0330

    add sass loader and css reset

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script>
+import "modern-css-reset";
 export default {
   name: "App",
 };
@@ -15,8 +16,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
